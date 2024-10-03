450af5f03cc9a2a0ceeec71a7d49e796478e5c39
Clark <jeffjrcl@UMICH.EDU>
2024-10-03 13:58:42 -0400

    Updated Hello.java code on master

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 // JavaScript source codepublic class Hello {
-public static void main(String[] args) {
+public class Hello {
+    public static void main(String[] args){
 for(int i=1;i<=10;i++) {
 System.out.println("Hello World..."+i);
 //changes to hello world.java
